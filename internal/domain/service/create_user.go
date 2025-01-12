0785f2cf635cdef1321d326fe5d33585ab406791
Walber Vaz <wvs.walber@gmail.com>
2025-01-12 18:40:57 -0300

    feat: handle create user usage domain service

--- internal/domain/service/create_user.go
+++ internal/domain/service/create_user.go
@@ -1,16 +1,17 @@
-package domain
+package service
 
 import (
 	"fmt"
 	"go.uber.org/zap"
 	"server-go/internal/config/logger"
 	"server-go/internal/config/rest_errors"
+	"server-go/internal/domain"
 )
 
-func (ud *userDomain) CreateUser() *rest_errors.RestErr {
+func (ud *userDomainService) CreateUser(userDomain domain.UserDomainInterface) *rest_errors.RestErr {
 	logger.Info("[INFO] - Sistema Fly - User domain - CreateUser", zap.String("journey", "CreateUser"))
-	ud.EncryptPassword()
+	userDomain.EncryptPassword()
 
-	fmt.Println(ud)
+	fmt.Println(userDomain.GetPassword())
 	return nil
 }
