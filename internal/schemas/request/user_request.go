75e9ac22e67836ac0bdf6b3afeee300ec4097181
Walber Vaz <wvs.walber@gmail.com>
2025-01-12 16:20:53 -0300

    feat: add field phone

--- internal/schemas/request/user_request.go
+++ internal/schemas/request/user_request.go
@@ -5,6 +5,7 @@ type UserRequest struct {
 	LastName  string `json:"last_name" binding:"required,min=2,max=80"`
 	Email     string `json:"email" binding:"required,email"`
 	Password  string `json:"password" binding:"required,min=8,containsany=!@#$%^&*()_+"`
+	Phone     string `json:"phone" binding:"required,min=12,max=12,numeric"`
 	Role      string `json:"role,omitempty" binding:"omitempty,oneof=ADMIN USER"`
 	IsActive  bool   `json:"is_active,omitempty" binding:"omitempty,boolean"`
 }
--- internal/schemas/response/user_response.go
+++ internal/schemas/response/user_response.go
@@ -6,6 +6,7 @@ type UserResponse struct {
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	Role      string `json:"role"`
+	Phone     string `json:"phone"`
 	IsActive  bool   `json:"is_active"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
