b365d8823d9585e4c5016a66893e94f23dec4fa3
Walber Vaz <wvs.walber@gmail.com>
2025-01-16 18:17:08 -0300

    feat: enhance MongoDB connection function with error handling and context support

--- internal/config/database/mongodb/mongodb_colletion.go
+++ internal/config/database/mongodb/mongodb_colletion.go
@@ -9,16 +9,20 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-func InitConnection(config *config.DB) {
-	ctx := context.Background()
-	client, err := mongo.Connect(options.Client().ApplyURI(config.Host + ":" + config.Port))
+func NewMongoDBConnection(ctx context.Context, config *config.DB) (*mongo.Database, error) {
+	logger.Info("[INFO] Conectando com o MongoDB")
 
+	client, err := mongo.Connect(options.Client().ApplyURI(config.Host + ":" + config.Port))
 	if err != nil {
-		panic(err)
+		logger.Error("[ERROR] Erro ao conectar com o MongoDB", err)
+		return nil, err
 	}
 
 	if err := client.Ping(ctx, nil); err != nil {
-		panic(err)
+		logger.Error("[ERROR] Erro ao pingar o MongoDB", err)
+		return nil, err
 	}
-	logger.Info("[INFO] Connected to MongoDB")
+
+	logger.Info("[INFO] Conexão com o MongoDB realizada com sucesso")
+	return client.Database(config.Name), nil
 }
